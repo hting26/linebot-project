e6f254d98a76112656fb4e8e23a4a1100c626b6f
hting26 <26hting@gmail.com>
2021-12-31 05:04:19 +0800

    性別不限測試完成
    剩性別不限+有體型 性別不限+無體型 無體型(+性別有限

--- index.js
+++ index.js
@@ -42,11 +42,15 @@ bot.on('message', async (event) => {
   }
 
   // 3.使用者選擇性別後，請使用者選體型
-  if ((event.message.text === '不限') || (event.message.text === 'M') || (event.message.text === 'F')) {
+  if ((event.message.text === 'M') || (event.message.text === 'F')) {
     event.reply(findBodytype)
     eventData[event.source.userId].animalSex = event.message.text
   }
-
+  // 如果性別不限
+  if (event.message.text === '不限') {
+    event.reply(findBodytype)
+  }
+  // 紀錄體型
   if ((event.message.text === '都可以') || (event.message.text === 'BIG') || (event.message.text === 'MEDIUM') || (event.message.text === 'SMALL')) {
     eventData[event.source.userId].bodytype = event.message.text
     console.log(eventData)
@@ -56,7 +60,180 @@ bot.on('message', async (event) => {
     const results = []
     const { data } = await axios.get('https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL')
     for (const info of data) {
-      if ((eventData[event.source.userId]?.animalKind === info.animal_kind) && (eventData[event.source.userId]?.animalSex === info.animal_sex) && (eventData[event.source.userId]?.bodytype === info.animal_bodytype) && (eventData[event.source.userId]?.shelter === info.shelter_name)) {
+      if (eventData[event.source.userId].animalSex === '') {
+        if ((eventData[event.source.userId]?.animalKind === info.animal_kind) && (eventData[event.source.userId]?.bodytype === info.animal_bodytype) && (eventData[event.source.userId]?.shelter === info.shelter_name)) {
+          results.push(info.animal_subid)
+          flexTemplate.contents.contents.push({
+            type: 'bubble',
+            size: 'kilo',
+            hero: {
+              type: 'image',
+              url: info.album_file || 'https://images.unsplash.com/photo-1450778869180-41d0601e046e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1286&q=80',
+              size: 'full',
+              aspectMode: 'cover',
+              aspectRatio: '20:17'
+            },
+            body: {
+              type: 'box',
+              layout: 'vertical',
+              contents: [
+                {
+                  type: 'text',
+                  text: `收容編號 ${info.animal_subid}` || '-',
+                  size: 'sm',
+                  wrap: true
+                },
+                {
+                  type: 'box',
+                  layout: 'vertical',
+                  margin: 'lg',
+                  spacing: 'sm',
+                  contents: [
+                    {
+                      type: 'box',
+                      layout: 'baseline',
+                      spacing: 'sm',
+                      contents: [
+                        {
+                          type: 'text',
+                          text: '性別',
+                          color: '#aaaaaa',
+                          size: 'sm',
+                          flex: 1
+                        },
+                        {
+                          type: 'text',
+                          text: info.animal_sex || '-',
+                          wrap: true,
+                          color: '#666666',
+                          size: 'sm',
+                          flex: 5
+                        }
+                      ]
+                    },
+                    {
+                      type: 'box',
+                      layout: 'baseline',
+                      spacing: 'sm',
+                      contents: [
+                        {
+                          type: 'text',
+                          text: '體型',
+                          color: '#aaaaaa',
+                          size: 'sm',
+                          flex: 1
+                        },
+                        {
+                          type: 'text',
+                          text: info.animal_bodytype || '-',
+                          wrap: true,
+                          color: '#666666',
+                          size: 'sm',
+                          flex: 5
+                        }
+                      ]
+                    },
+                    {
+                      type: 'box',
+                      layout: 'baseline',
+                      spacing: 'sm',
+                      contents: [
+                        {
+                          type: 'text',
+                          text: '毛色',
+                          color: '#aaaaaa',
+                          size: 'sm',
+                          flex: 1
+                        },
+                        {
+                          type: 'text',
+                          text: info.animal_colour || '-',
+                          wrap: true,
+                          color: '#666666',
+                          size: 'sm',
+                          flex: 5
+                        }
+                      ]
+                    },
+                    {
+                      type: 'box',
+                      layout: 'baseline',
+                      spacing: 'sm',
+                      contents: [
+                        {
+                          type: 'text',
+                          text: '狀態描述',
+                          color: '#aaaaaa',
+                          size: 'sm',
+                          flex: 2
+                        },
+                        {
+                          type: 'text',
+                          text: info.animal_remark || '-',
+                          wrap: true,
+                          color: '#666666',
+                          size: 'sm',
+                          flex: 5
+                        }
+                      ]
+                    },
+                    {
+                      type: 'box',
+                      layout: 'baseline',
+                      spacing: 'sm',
+                      contents: [
+                        {
+                          type: 'text',
+                          text: '開放認養時間',
+                          color: '#aaaaaa',
+                          size: 'sm',
+                          flex: 4
+                        },
+                        {
+                          type: 'text',
+                          text: info.animal_opendate || '-',
+                          wrap: true,
+                          color: '#666666',
+                          size: 'sm',
+                          flex: 5
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ],
+              paddingBottom: '6px'
+            },
+            footer: {
+              type: 'box',
+              layout: 'vertical',
+              spacing: 'none',
+              contents: [
+                {
+                  type: 'button',
+                  style: 'primary',
+                  action: {
+                    type: 'uri',
+                    label: '前往網站',
+                    uri: encodeURI(`https://asms.coa.gov.tw/amlapp/App/AnnounceList.aspx?Id=${info.animal_id}&AcceptNum=${info.animal_subid}&PageType=Adopt`)
+                  },
+                  height: 'sm',
+                  color: '#b5927f'
+                }
+              ],
+              flex: 0
+            },
+            styles: {
+              footer: {
+                separator: false
+              }
+            }
+          })
+          if (results.length >= 12) {
+            break
+          }
+        }
+      } else if ((eventData[event.source.userId]?.animalKind === info.animal_kind) && (eventData[event.source.userId]?.animalSex === info.animal_sex) && (eventData[event.source.userId]?.bodytype === info.animal_bodytype) && (eventData[event.source.userId]?.shelter === info.shelter_name)) {
         results.push(info.animal_subid)
         flexTemplate.contents.contents.push({
           type: 'bubble',
